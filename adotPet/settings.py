5089c2486ba385f09ba3448536938309d46302dc
Pedro Lucas <pedrolucasmoraisborges@gmail.com>
2023-08-22 17:58:07 -0300

    users_abstract

--- adotPet/settings.py
+++ adotPet/settings.py
@@ -131,3 +131,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'auth_user.User'
