422cd458575f24bbd843775caa0bcfa55f4b754e
Lucas Borges de Almeida <borgeslucas19000@gmail.com>
2023-09-20 15:51:31 -0300

    Front end da tela de cadastro v1.0

--- pages/forms.py
+++ pages/forms.py
@@ -17,20 +17,25 @@ choicesRacas = [
     ('Golden Retriever', 'Golden Retriever'),
     ('Caramelo', 'Caramelo')]
 
+choicesSexo = [
+    ('Macho', 'Macho'),
+    ('Fêmea', 'Fêmea')
+]
+
 class CadastrarPetForm(forms.ModelForm):
-    nome = forms.CharField(label='Nome')
-    idade = forms.IntegerField(label='Idade')
-    raca = forms.ChoiceField(label='Raça', choices=[('', 'Selecione a Raça')]+choicesRacas)
-    desc = forms.CharField(label='Descrição')
-    obs = forms.CharField(label='Observações')
-    sexo = forms.ChoiceField(label='Sexo', choices=[('', 'Selecione o sexo...')] + choices)
+    nome = forms.CharField(label='Nome do seu Pet:', widget=forms.TextInput(attrs={'class':'input-pet-information', 'placeholder':'Digite o nome do pet aqui'}))
+    raca = forms.ChoiceField(label='Raça', choices=[('', 'Selecione a Raça')]+choicesRacas, widget=forms.Select(attrs={'class':'input-pet-information'}))
+    desc = forms.CharField(label='Descrição do animal:', widget=forms.TextInput(attrs={'class':'input-pet-information', 'placeholder': 'Digite a descrição do pet aqui'}))
+    idade = forms.IntegerField(label='Idade', widget=forms.NumberInput(attrs={'class':'input-pet-information input-idade', 'placeholder':'Idade em anos'}))
+    obs = forms.CharField(label='Observações', widget=forms.TextInput(attrs={'class':'input-pet-information', 'placeholder':'Digite as observações aqui'}))
+    sexo = forms.ChoiceField(label='Sexo', choices=choicesSexo, widget=forms.RadioSelect(attrs={'class':'radio-inputs'}))
 
     class Meta:
         model = Pet
-        fields = ['nome', 'idade', 'desc', 'obs', 'sexo','raca',]
+        fields = ['nome', 'desc', 'obs', 'idade', 'sexo','raca',]
 
 class CadastroImagemForm(forms.ModelForm):
-    imagem = forms.ImageField()
+    imagem = forms.ImageField(required=True)
     class Meta:
         model = ImagemPet
         fields = ['imagem',]
---
+++ static/js/check_radio.js
@@ -0,0 +1,26 @@
+console.log("Opa")
+
+let machoCamp = document.getElementById('id_sexo_0')
+let femeaCamp = document.getElementById('id_sexo_1')
+
+let machoButton = document.getElementById('machoButton')
+let femeaButton = document.getElementById('femeaButton')
+
+machoButton.addEventListener('click', triggerMacho)
+femeaButton.addEventListener('click', triggerFemea)
+
+function triggerMacho(){
+    if(machoCamp.checked == false){
+        machoCamp.checked = true
+        machoButton.classList.add('active-radio')
+        femeaButton.classList.remove('active-radio')
+    }
+}
+
+function triggerFemea(){
+    if(femeaCamp.checked == false){
+        femeaCamp.checked = true
+        machoButton.classList.remove('active-radio')
+        femeaButton.classList.add('active-radio')
+    }
+}
