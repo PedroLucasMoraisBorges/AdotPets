dc3080c38193052dde9e0e355b0a615a1538524a
Lucas Borges de Almeida <borgeslucas19000@gmail.com>
2023-09-26 01:01:08 -0300

    Fix: Cadastrar Imagens 100% OK, falta preview

--- static/js/jquery.formset.js
+++ static/js/jquery.formset.js
@@ -237,7 +237,7 @@
         addContainerClass: null,         // Container CSS class for the add link
         deleteContainerClass: null,      // Container CSS class for the delete link
         addCssClass: 'add-row',          // CSS class applied to the add link
-        deleteCssClass: 'delete-row',    // CSS class applied to the delete link
+        deleteCssClass: 'delete',    // CSS class applied to the delete link
         formCssClass: 'dynamic-form',    // CSS class applied to each form in a formset
         extraClasses: [],                // Additional CSS classes, which will be applied to each form in turn
         keepFieldValues: '',             // jQuery selector for fields whose values should be kept when the form is cloned
